01845c6b7e7c4d3b3bd76f8ab7782e8cb1399ad3
Azka Noreen <azka.noreen6@gmail.com>
2023-01-06 14:12:38 +0500

    Toolbar add sign added , plus menu done

--- app/src/main/java/azka/noreen/filemanager/BrowseFilesActivity.java
+++ app/src/main/java/azka/noreen/filemanager/BrowseFilesActivity.java
@@ -1,11 +1,17 @@
 package azka.noreen.filemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +23,7 @@ public class BrowseFilesActivity extends AppCompatActivity {
     RecyclerView recycleView;
     TextView text;
     ArrayList<StorageItems> studentArrayList=new ArrayList<>();
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,12 @@ public class BrowseFilesActivity extends AppCompatActivity {
         recycleView=findViewById(R.id.rv);
         text=findViewById(R.id.text);
         text.setVisibility(View.GONE);
+        toolbar=findViewById(R.id.toolba);
+
+        setSupportActionBar(toolbar); //set toolbar to act as action bar
+        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_baseline_add_circle_24);
+        toolbar.setOverflowIcon(drawable);
+
 
         Intent intent=getIntent();
         String rootPath=intent.getStringExtra("RootPath");
@@ -50,5 +63,23 @@ public class BrowseFilesActivity extends AppCompatActivity {
         recycleView.setLayoutManager(new LinearLayoutManager(this));
         rva.setData(studentArrayList);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu ,this adds items to action bar but here in toolbar
+        getMenuInflater().inflate(R.menu.filemenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int menuItemId=item.getItemId();
+        if(menuItemId==R.id.folder){
+            Toast.makeText(this, "Folder Clicked", Toast.LENGTH_SHORT).show();
+        }else if(menuItemId==R.id.file){
+            Toast.makeText(this, "File Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
